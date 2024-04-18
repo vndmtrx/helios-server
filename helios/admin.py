cc4fe768125b2ed735d77f164344a71cf99a0352
Eduardo N.S.R <vndmtrx@duck.com>
2024-04-18 11:18:00 -0300

    Implantação de Admin Models do Django Admin para o projeto

---
+++ helios/admin.py
@@ -0,0 +1,38 @@
+# Importado de: https://github.com/ifsc/helios-server/
+
+from django.contrib import admin
+
+from helios.models import CastVote, Election, Trustee, Voter
+
+
+class CastVoteAdmin(admin.ModelAdmin):	
+	fields = ('voter', 'vote_hash', 'cast_at', 'cast_ip')
+	readonly_fields = ('voter', 'vote_hash', 'cast_at', 'cast_ip')
+	list_display = ('voter', 'vote_hash', 'cast_at', 'cast_ip')
+
+
+class ElectionAdmin(admin.ModelAdmin):
+	fields = ('admin', 'short_name', 'name', 'election_type',
+	'openreg', 'use_voter_aliases', 'randomize_answer_order', 
+	'frozen_at', 'tallying_started_at', 'tallying_finished_at',
+	'help_email', 'election_info_url', 'result', 'featured_p')
+	readonly_fields = ('admin', 'short_name', 'name', 'election_type',
+	'openreg', 'use_voter_aliases', 'randomize_answer_order', 
+	'frozen_at', 'tallying_started_at', 'tallying_finished_at', 'help_email',
+	'election_info_url', 'result')
+	list_display = ('admin', 'name', 'election_type', 'featured_p')
+
+
+class TrusteeAdmin(admin.ModelAdmin):	
+	readonly_fields = ('uuid', 'election','name', 'email', 'secret',)
+	list_display = ('uuid', 'election', 'name', 'email', 'secret',)
+
+
+class VoterAdmin(admin.ModelAdmin):
+	readonly_fields = ('vote', 'vote_hash', 'user', 'cast_at')
+
+
+admin.site.register(CastVote, CastVoteAdmin)
+admin.site.register(Election, ElectionAdmin)
+admin.site.register(Trustee, TrusteeAdmin)
+admin.site.register(Voter, VoterAdmin)
---
+++ helios_auth/admin.py
@@ -0,0 +1,10 @@
+# Importado de: https://github.com/ifsc/helios-server/
+
+from django.contrib import admin
+from helios.models import User
+
+class UserAdmin(admin.ModelAdmin):      
+    exclude = ('info', 'token')
+    list_display = ('name', 'user_id')
+
+admin.site.register(User, UserAdmin)
