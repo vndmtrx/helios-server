646b66db4d48d3b709d8f2df565bb7908352cc6b
Eduardo N.S.R <vndmtrx@duck.com>
2024-04-18 16:36:30 -0300

    Ajustes no `reset.sh`

--- reset.sh
+++ reset.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 set -e  # Exit immediately if a command exits with a non-zero status.
 
-echo "Importando variáveis de ambiente..."
+echo "#### Importando variáveis de ambiente..."
 source .env
 
-echo "Recriandbo banco de dados..."
+echo "#### Recriandbo banco de dados..."
 sudo -u postgres dropdb --if-exists "${DB_NAME}"
 sudo -u postgres createdb "${DB_NAME}"
+
+echo "#### Criando usuário, se não existir..."
 cat <<EOF | sudo -u postgres psql
 DO
-$$do$$
+\$do\$
 BEGIN
    IF NOT EXISTS ( SELECT FROM pg_roles WHERE rolname = '${DB_USER}' ) THEN
       CREATE USER ${DB_USER} WITH SUPERUSER PASSWORD '${DB_PWD}';
    END IF;
 END
-$$do$$;
+\$do\$;
 EOF
 
+echo "#### Aplicação dos migrations iniciais no banco de dados..."
 python manage.py makemigrations
 python manage.py migrate
 
 if [ -n "${DJANGO_SUPERUSER_USERNAME}" ]; then
+echo "#### Criação do superusuário do Django Admin..."
 cat <<EOF | python manage.py shell
 from django.contrib.auth.models import User as DjangoAdminUser;
 from django.contrib.auth.hashers import make_password; 
@@ -35,6 +39,7 @@ DjangoAdminUser.objects.create(
 EOF
 fi
 
+echo "#### Criação de um usuário no Helios..."
 cat <<EOF | python manage.py shell
 from helios_auth.models import User as HeliosUser
 HeliosUser.objects.create(
