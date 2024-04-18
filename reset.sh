60570a28b58fb651285bca7ab88f0c8adb72feb3
Eduardo N.S.R <vndmtrx@duck.com>
2024-04-18 11:11:32 -0300

    Alterado `reset.sh` para recriar corretamente o banco de dados do Helios

--- reset.sh
+++ reset.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 set -e  # Exit immediately if a command exits with a non-zero status.
-dropdb helios
-createdb helios
+sudo -u postgres dropdb --if-exists helios
+sudo -u postgres createdb helios
+sudo -u postgres psql -c "CREATE USER helios WITH SUPERUSER PASSWORD 'heliosteste';"
+
 python manage.py makemigrations
 python manage.py migrate
-echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='ben@adida.net', info={'name':'Ben Adida'})" | python manage.py shell
+
+python manage.py createsuperuser
+
+cat <<EOF | python manage.py shell
+from helios_auth.models import User
+User.objects.create(
+    user_type='password', 
+    name='Dudu', 
+    user_id='vndmtrx@duck.com', 
+    info={
+        'name':'Dudu', 
+        'password':'semsenha'
+    }
+)
+EOF
