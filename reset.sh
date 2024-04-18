d472091210146b2350c7e5d4263290ade7ed374c
Eduardo N.S.R <vndmtrx@duck.com>
2024-04-18 16:25:01 -0300

    Ajuste do reset para usar as variáveis de ambiente

--- reset.sh
+++ reset.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
 set -e  # Exit immediately if a command exits with a non-zero status.
-sudo -u postgres dropdb --if-exists helios
-sudo -u postgres createdb helios
-sudo -u postgres psql -c "CREATE USER helios WITH SUPERUSER PASSWORD 'heliosteste';"
+
+echo "Importando variáveis de ambiente..."
+source .env
+
+echo "Recriandbo banco de dados..."
+sudo -u postgres dropdb --if-exists "${DB_NAME}"
+sudo -u postgres createdb "${DB_NAME}"
+cat <<EOF | sudo -u postgres psql
+DO
+$$do$$
+BEGIN
+   IF NOT EXISTS ( SELECT FROM pg_roles WHERE rolname = '${DB_USER}' ) THEN
+      CREATE USER ${DB_USER} WITH SUPERUSER PASSWORD '${DB_PWD}';
+   END IF;
+END
+$$do$$;
+EOF
 
 python manage.py makemigrations
 python manage.py migrate
 
-python manage.py createsuperuser
+if [ -n "${DJANGO_SUPERUSER_USERNAME}" ]; then
+cat <<EOF | python manage.py shell
+from django.contrib.auth.models import User as DjangoAdminUser;
+from django.contrib.auth.hashers import make_password; 
+DjangoAdminUser.objects.create(
+    is_staff=True, 
+    is_superuser=True, 
+    username='${DJANGO_SUPERUSER_USERNAME}', 
+    email='${DJANGO_SUPERUSER_EMAIL}', 
+    password=make_password('${DJANGO_SUPERUSER_PASSWORD}')
+)
+EOF
+fi
 
 cat <<EOF | python manage.py shell
-from helios_auth.models import User
-User.objects.create(
+from helios_auth.models import User as HeliosUser
+HeliosUser.objects.create(
     user_type='password', 
     name='Dudu', 
     user_id='vndmtrx@duck.com', 
