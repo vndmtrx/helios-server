b6f900f88ea9f68105dae94b213f67763ab76672
Eduardo N. S. R. <ens.rolim@ufu.br>
2024-05-22 15:17:13 -0300

    Ajuste

--- helios/views.py
+++ helios/views.py
@@ -1327,10 +1327,10 @@ def voters_email(request, election):
   if not VOTERS_EMAIL:
     return HttpResponseRedirect(settings.SECURE_URL_HOST + reverse(url_names.election.ELECTION_VIEW, args=[election.uuid]))
   TEMPLATES = [
-    ('vote', 'Time to Vote'),
-    ('simple', 'Simple'),
-    ('info', 'Additional Info'),
-    ('result', 'Election Result')
+    ('vote', 'Hora de Votar'),
+    ('simple', 'Simples'),
+    ('info', 'Informações Adicionais'),
+    ('result', 'Resultado')
     ]
 
   template = request.GET.get('template', 'vote')
